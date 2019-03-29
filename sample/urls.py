56c7418b30433a15ae547ff4fcff821e33c94492
ParkJiSu28 <qkrwltn28@gmail.com>
2019-03-29 20:22:17 +0900

    1주차 실습

---
+++ sample/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url 
+from rest_framework.routers import DefaultRouter 
+from .views import PostViewSet  
+router = DefaultRouter() 
+router.register(r'post', PostViewSet)  
+urlpatterns = [     url(r'', include(router.urls)), 
+        ]
